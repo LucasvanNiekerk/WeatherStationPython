51d0abcc01e305a2b7d8fe86ab3e0b7412c03f9e
mort237d <43375205+mort237d@users.noreply.github.com>
2019-11-28 09:12:47 +0100

    Update WeatherStationPython.py
    small changes

--- WeatherStationPython.py
+++ WeatherStationPython.py
@@ -7,11 +7,9 @@ BROADCAST_TO_PORT = 7000
 
 s = SenseHat()
 
-s.clear()
+s.low_light = True
 
-t = s.get_temperature()
-h = s.get_humidity()
-p = s.get_pressure()
+s.clear()
 
 g = (0, 255, 0)  # Green
 e = (0, 0, 0)  # Empty
@@ -34,8 +32,8 @@ arrow_up = [
     r, r, r, r, r, r, r, r,
     e, e, e, r, r, e, e, e,
     e, e, r, r, r, r, e, e,
-    e, r, e, r, r, e, r, e,
-    r, e, e, r, r, e, e, r,
+    e, r, r, r, r, r, r, e,
+    r, r, e, r, r, e, r, r,
     e, e, e, r, r, e, e, e,
     e, e, e, r, r, e, e, e,
     e, e, e, r, r, e, e, e
@@ -65,21 +63,28 @@ def upload_animation():
     s.clear()
     sleep(1)
 
-mySocket = socket(AF_INET, SOCK_DGRAM)
-mySocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+def upload_animation2():
+  for i in range(7, -1, -1):
+    s.set_pixels(arrow_up)
+    sleep(.2)
+    for j in range(8):
+      s.set_pixel(j, i, e)
+    sleep(.2)
 
-while True:
-    #download_animation()
-    #upload_animation()
-    #s.set_pixels(stop)
-    #s.load_image("kirby.png")
 
-    id = 1
-    raspberry_id = "RPI404"
+def UDP_Sender():
+    raspberry_id = "TestData22"
     temperature = str(round(s.get_temperature(), 2))
     humidity = str(round(s.get_humidity(), 2))
     time = str(datetime.now())
 
-    data = "{ Id = " + str(id) + ", RaspberryId = " + raspberry_id + ", Temperature = " + temperature + ", Humidity = " + humidity + ", TimeStamp " + time + " }"
+    data = "{\"raspberryId\":\"" + raspberry_id + "\",\"temperature\":\"" + temperature + "\",\"humidity\":\"" + humidity + "\",\"timeStamp\":\"" + time + "\"}"
     mySocket.sendto(bytes(data.encode("UTF-8")), ('<broadcast>', BROADCAST_TO_PORT))
     sleep(1)
+
+mySocket = socket(AF_INET, SOCK_DGRAM)
+mySocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+
+while True:
+    upload_animation2()
+    UDP_Sender()
